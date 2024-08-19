8a33bdcc8fba91c22744d2e85ffe0ce418ac51da
Gabey <130712384+Gabey4Heal@users.noreply.github.com>
2024-08-19 17:25:17 -0300

    Update Beaconproject.ino

--- Beaconproject.ino
+++ Beaconproject.ino
@@ -21,97 +21,95 @@ float calculateDistance(int rssi) {
     }
     return pow(10, ((referenceRSSI - rssi) / (10 * pathLossExponent)));
 }
+
 typedef struct {
-      uint16_t manufacturerId;
-      uint8_t subType;
-      uint8_t subTypeLength;
-      uint8_t proximityUUID[16];
-      uint16_t major;
-      uint16_t minor;
-      int8_t signalPower;
+    uint16_t manufacturerId;
+    uint8_t subType;
+    uint8_t subTypeLength;
+    uint8_t proximityUUID[16];
+    uint16_t major;
+    uint16_t minor;
+    int8_t signalPower;
 } __attribute__((packed)) m_beaconData;
 
-
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
-   void onResult(BLEAdvertisedDevice advertisedDevice) override {
-       if (advertisedDevice.haveManufacturerData()) {
-           String strManufacturerData = advertisedDevice.getManufacturerData();
-           uint8_t cManufacturerData[100];
-           int dataLength = strManufacturerData.length();
+    void onResult(BLEAdvertisedDevice advertisedDevice) override {
+        if (advertisedDevice.haveManufacturerData()) {
+            String strManufacturerData = advertisedDevice.getManufacturerData();
+            uint8_t cManufacturerData[100];
+            int dataLength = strManufacturerData.length();
 
-           if (dataLength >= 25) {
-               BLEBeacon oBeacon;
-               oBeacon.setData(strManufacturerData);
-               uint16_t minor = oBeacon.getMinor();
-               int rssi = advertisedDevice.getRSSI();
-               float distance = calculateDistance(rssi) * 10;
-              
+            if (dataLength >= 25) {
+                BLEBeacon oBeacon;
+                oBeacon.setData(strManufacturerData);
+                uint16_t major = oBeacon.getMajor(); // Obter Major corretamente
+                uint16_t minor = oBeacon.getMinor(); // Obter Minor corretamente
+                int rssi = advertisedDevice.getRSSI();
+                float distance = calculateDistance(rssi) * 10;
 
-               Serial.printf("Distância Estimada: %.2f cm\n", distance);
-               // Corrigido: substituindo MACAddress por macAddress
-               String macAddress = advertisedDevice.getAddress().toString();
-               Serial.printf("Endereço MAC: %s\n", macAddress.c_str());
-               Serial.printf("UUID: %s\n", oBeacon.getProximityUUID().toString().c_str());
-               Serial.printf("Major: %d (0x%04X)\n", swapEndian16(oBeacon.getMajor()), swapEndian16(oBeacon.getMajor()));
-               Serial.printf("Minor: %d (0x%04X)\n", minor, minor);
-               Serial.printf("RSSI: %d\n", rssi);
+                Serial.printf("Distância Estimada: %.2f cm\n", distance);
+                String macAddress = advertisedDevice.getAddress().toString();
+                Serial.printf("Endereço MAC: %s\n", macAddress.c_str());
+                Serial.printf("UUID: %s\n", oBeacon.getProximityUUID().toString().c_str());
+                Serial.printf("Major: %d (0x%04X)\n", major, major);
+                Serial.printf("Minor: %d (0x%04X)\n", minor, minor);
+                Serial.printf("RSSI: %d\n", rssi);
 
-               
-               if (minor == 30000) {
-                   String deviceName = advertisedDevice.haveName() ? advertisedDevice.getName() : "Desconhecido";
+                if (minor == 30000) {
+                    String deviceName = advertisedDevice.haveName() ? advertisedDevice.getName() : "Desconhecido";
 
-                   Serial.println("====================================");
-                   Serial.printf("Endereço MAC: %s\n", macAddress.c_str());
-                   Serial.printf("Nome do Dispositivo: %s\n", deviceName.c_str());
-                   Serial.printf(
-                       "ID: %04X \nMajor: %d (0x%04X)",
-                       oBeacon.getManufacturerId(),
-                       swapEndian16(oBeacon.getMajor()), swapEndian16(oBeacon.getMajor()),
-                       minor, minor,
-                       oBeacon.getProximityUUID().toString().c_str(),
-                       oBeacon.getSignalPower()
-                   );
+                    Serial.println("====================================");
+                    Serial.printf("Endereço MAC: %s\n", macAddress.c_str());
+                    Serial.printf("Nome do Dispositivo: %s\n", deviceName.c_str());
+                    Serial.printf(
+                        "ID: %04X \nMajor: %d (0x%04X) \nMinor: %d (0x%04X) \nUUID: %s \nSignal Power: %d\n",
+                        oBeacon.getManufacturerId(),
+                        major, major,
+                        minor, minor,
+                        oBeacon.getProximityUUID().toString().c_str(),
+                        oBeacon.getSignalPower()
+                    );
 
-                   if (advertisedDevice.getServiceUUID().equals(BLEUUID(TARGET_UUID))) {
-                       Serial.print("UUID : ");
-                       Serial.println(oBeacon.getProximityUUID().toString().c_str());
-                       Serial.print("RSSI : ");
-                       Serial.println(rssi);
-                       Serial.print("Major : ");
-                       Serial.println(oBeacon.getMajor(), HEX);
-                       Serial.print("Minor : ");
-                       Serial.println(oBeacon.getMinor(), HEX);
-                       Serial.print("Distance : ");
-                       Serial.println(distance, 2);
-                       Serial.println("");
+                    if (advertisedDevice.getServiceUUID().equals(BLEUUID(TARGET_UUID))) {
+                        Serial.print("UUID : ");
+                        Serial.println(oBeacon.getProximityUUID().toString().c_str());
+                        Serial.print("RSSI : ");
+                        Serial.println(rssi);
+                        Serial.print("Major : ");
+                        Serial.println(major, HEX);
+                        Serial.print("Minor : ");
+                        Serial.println(minor, HEX);
+                        Serial.print("Distance : ");
+                        Serial.println(distance, 2);
+                        Serial.println("");
 
-                       if (dataLength > sizeof(cManufacturerData)) {
-                           dataLength = sizeof(cManufacturerData);
-                       }
-                       memcpy(cManufacturerData, strManufacturerData.c_str(), dataLength);
-                   }
-                   
-                   if (advertisedDevice.haveServiceData()) {
-                       String serviceDataStr = advertisedDevice.getServiceData();
-                       std::string serviceData(serviceDataStr.c_str());
-                       uint8_t* serviceDataBytes = (uint8_t*)serviceData.c_str();
-                       int serviceDataLength = serviceData.length();
+                        if (dataLength > sizeof(cManufacturerData)) {
+                            dataLength = sizeof(cManufacturerData);
+                        }
+                        memcpy(cManufacturerData, strManufacturerData.c_str(), dataLength);
+                    }
 
-                       if (serviceDataLength > 0) {
-                           uint8_t batteryLevel = serviceDataBytes[0];
-                           Serial.printf("Porcentagem da Bateria: %d%%\n", batteryLevel);
-                       } else {
-                           Serial.println("Dados de bateria não encontrados.");
-                       }
-                   } else {
-                       Serial.println("Serviço de bateria não encontrado.");
-                   }
+                    if (advertisedDevice.haveServiceData()) {
+                        String serviceDataStr = advertisedDevice.getServiceData();
+                        std::string serviceData(serviceDataStr.c_str());
+                        uint8_t* serviceDataBytes = (uint8_t*)serviceData.c_str();
+                        int serviceDataLength = serviceData.length();
 
-                   Serial.println("====================================");
-               }
-           }
-       }
-   }
+                        if (serviceDataLength > 0) {
+                            uint8_t batteryLevel = serviceDataBytes[0];
+                            Serial.printf("Porcentagem da Bateria: %d%%\n", batteryLevel);
+                        } else {
+                            Serial.println("Dados de bateria não encontrados.");
+                        }
+                    } else {
+                        Serial.println("Serviço de bateria não encontrado.");
+                    }
+
+                    Serial.println("====================================");
+                }
+            }
+        }
+    }
 };
 
 uint16_t swapEndian16(uint16_t value) {
